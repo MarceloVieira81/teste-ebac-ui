da9f9e24b0db048992192e3605f8494f85a3fe9d
Marcelo Vieira <mv.marcelovieira.0@gmail.com>
2022-09-28 13:59:39 -0300

    endereço de entraga

--- cypress/e2e/e2e-endereco.cy.js
+++ cypress/e2e/e2e-endereco.cy.js
@@ -17,7 +17,7 @@ const dadosEndereco = require('../fixtures/endereco.json')
          cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.') 
     });
 
-    it.only('Deve fazer cadastro de faturamento com sucesso - usando arquivo de dados', () => {
+    it('Deve fazer cadastro de faturamento com sucesso - usando arquivo de dados', () => {
         EnderecoPage.editarEnderecoFaturamento(
             dadosEndereco[1].nome,
             dadosEndereco[1].sobrenome,
@@ -32,5 +32,21 @@ const dadosEndereco = require('../fixtures/endereco.json')
             dadosEndereco[1].email
             )    
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.') 
+   });
+
+   it.only('Deve fazer cadastro de endereço de entrega com sucesso', () => {
+        EnderecoPage.editarEnderecoEntrega(
+            dadosEndereco[2].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep
+        )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+
    });
  });
